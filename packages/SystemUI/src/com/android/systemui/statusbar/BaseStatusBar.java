cef0e2b5d24986ce67524826363de9241ab1d14c
Sirhc95 <chriscrawf95@gmail.com>
2016-04-20 16:52:26 -0700

    SystemUI: No need for If on gesture overlay

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2677,11 +2677,9 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_METHOD)
     protected void addGestureAnywhereView() {
-        if (mGestureAnywhereView == null) {
-            mGestureAnywhereView = (GestureAnywhereView)View.inflate(
-                    mContext, R.layout.gesture_anywhere_overlay, null);
-            mWindowManager.addView(mGestureAnywhereView, getGestureAnywhereViewLayoutParams(Gravity.LEFT));
-        }
+        mGestureAnywhereView = (GestureAnywhereView)View.inflate(
+                mContext, R.layout.gesture_anywhere_overlay, null);
+        mWindowManager.addView(mGestureAnywhereView, getGestureAnywhereViewLayoutParams(Gravity.LEFT));
         mGestureAnywhereView.setStatusBar(this);
     }
 
