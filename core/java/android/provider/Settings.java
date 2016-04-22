bdfee2c8bd51a4f386239facdf5be49ce85c30aa
Sirhc95 <chriscrawf95@gmail.com>
2016-04-22 15:04:15 -0700

    Heads Up Snooze 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4838,6 +4838,13 @@ public final class Settings {
         */
         public static final String DOZE_WAKEUP_DOUBLETAP = "doze_wakeup_doubletap";
         /**
+		 * Snooze time for heads up
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
